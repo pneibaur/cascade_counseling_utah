79863e9a25e452c67250dd1a60d168a715cd901e
Phil Neibaur <pneibaur+github@gmail.com>
2023-05-22 11:51:38 -0600

    WIP styling. WIP nav & footer

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -1,13 +1,40 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css"
 import "../styles/main.scss"
+import { Container, Row, Navbar, Nav, Col } from "react-bootstrap";
 
 
-const Layout = ({children}) => {
+const Layout = ({ children }) => {
     return (
-        <main>
-            {children}
-        </main>
+        <>
+            <div className="layout">
+                <Container>
+                    <header>
+                        <Navbar expand="sm">
+                            <Navbar.Toggle aria-controls="mainNav" />
+                            <Navbar.Collapse id="mainNav" className="justify-content-end"></Navbar.Collapse>
+                            <Nav>
+                                <Nav.Item>Home</Nav.Item>
+                            </Nav>
+                        </Navbar>
+                    </header>
+                </Container>
+            </div>
+            <main>
+                {children}
+            </main>
+            <div className="layout">
+                <Container>
+                    <footer id="mainFooter" className="pageFooter">
+                        <Row className="justify-content-center">
+                            <Col>
+                                <span>footer goes here</span>
+                            </Col>
+                        </Row>
+                    </footer>
+                </Container>
+            </div>
+        </>
     )
 }
 
